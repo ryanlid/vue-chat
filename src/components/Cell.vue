8ef760da00dff65cd7b632751d840eaea1c9cdba
ryanlid <ryanlid@live.com>
2019-08-24 15:03:45 +0800

    feat: 🎸 添加我的页面

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="cell">
-    <slot></slot>
+    <div>
+      <div class="title">{{ title }}</div>
+      <div v-if="label">{{ label }}</div>
+    </div>
+    <div class="value-wrap">
+      <div>{{ value }}</div>
+      <div class="icon iconfont icon-right" v-if="isLink"></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    value: {
+      type: String
+    },
+    label: {
+      type: String
+    },
+    isLink: {
+      type: Boolean
+    }
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+@import "../fonts/iconfont.css";
 .cell {
   background: #fff;
-  padding: 10px;
+  padding: 12px 0;
+  margin: 0 10px;
   border-bottom: 1px solid #efeff4;
+  display: flex;
+  justify-content: space-between;
+}
+.title {
+  font-weight: bold;
+}
+.value-wrap {
+  display: flex;
+  align-items: center;
+  color: #878889;
+  & .icon {
+    margin-left: 4px;
+  }
 }
 </style>
--- src/views/my/Index.vue
+++ src/views/my/Index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="list">
     <Header :left="false">我</Header>
+    <cell-group>
+      <cell title="姓名" value="Dong" is-link></cell>
+      <cell title="性别" value="男" is-link></cell>
+      <cell title="手机" value="13023456789" is-link></cell>
+      <cell title="关于" is-link></cell>
+      <cell title="退出"></cell>
+    </cell-group>
     <bottom-tab class="bottom"></bottom-tab>
   </div>
 </template>
@@ -8,6 +15,8 @@
 <script>
 import BottomTab from "../../components/BottomTab";
 import Header from "../../components/Header";
+import CellGroup from "../../components/CellGroup";
+import Cell from "../../components/Cell";
 
 export default {
   data() {
@@ -15,6 +24,8 @@ export default {
   },
   components: {
     Header,
+    CellGroup,
+    Cell,
     BottomTab
   },
   methods: {}
